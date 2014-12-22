c9163b54735c23559feed2700cd2076f6e84dd65
SrinivaC <srinivasan_chandrashekar@yahoo.com>
2014-12-22 10:37:50 +0530

    Update ByteHelper.java
    Srini- UMS Interview

--- src/main/java/no/ums/interview/ByteHelper.java
+++ src/main/java/no/ums/interview/ByteHelper.java
@@ -1,17 +1,23 @@
 package no.ums.interview;
 
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * Helper class to convert a byte[] to Hex String.
  *
  * Source: http://stackoverflow.com/questions/9655181/convert-from-byte-array-to-hex-string-in-java
  *
- * @author Ståle Undheim <su@ums.no>
+ * @author StÃ¥le Undheim <su@ums.no>
  */
 public interface ByteHelper {
 
     char[] HEX_ARRAY = "0123456789abcdef".toCharArray();
 
-    public static String bytesToHex(byte[] bytes) {
+    /*
+    public static  String bytesToHex(byte[] bytes) {
         char[] hexChars = new char[bytes.length * 2];
         for ( int j = 0; j < bytes.length; j++ ) {
             int v = bytes[j] & 0xFF;
@@ -19,6 +25,28 @@ public interface ByteHelper {
             hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
         }
         return new String(hexChars);
+    } */
+
+
+    public static String bytesToHex(byte[] bytes) throws NoSuchAlgorithmException{
+    	String input = new String (bytes);
+    	System.out.println("Input is "+ input);
+    	MessageDigest md = MessageDigest.getInstance("SHA-1");
+        md.update(bytes);
+ 
+        byte byteData[] = md.digest();
+
+        //convert the byte to hex format method 2
+        StringBuffer hexString = new StringBuffer();
+    	for (int i=0;i<byteData.length;i++) {
+    		String hex=Integer.toHexString(0xff & byteData[i]);
+   	     	if(hex.length()==1) hexString.append('0');
+   	     	hexString.append(hex);
+    	}
+    	System.out.println("Hex format : " + hexString.toString());
+    	return hexString.toString();
     }
+
+    
 
 }
